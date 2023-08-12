2488544d3aa3d532d8e5a57510068d82c5c048a6
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-12 14:41:20 +0200

    feat: add cart menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client';
 import Link from 'next/link';
 import { CartIcon, ChevronDownIcon, FavoriteIcon, ProfileIcon, SearchIcon } from './Icons';
+import { useState } from 'react';
+import CartMenu from './CartMenu';
 
 const Navbar = () => {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
   return (
     <section className="h-20 ">
       <div className="flex items-center justify-between py-2 px-7 h-full text-lg">
@@ -44,7 +49,12 @@ const Navbar = () => {
             <SearchIcon />
             <ProfileIcon />
             <FavoriteIcon />
-            <div className="relative">
+            <div
+              className="relative"
+              onClick={() => {
+                setisMenuOpen(!isMenuOpen);
+              }}
+            >
               <CartIcon />
               <span className="text-xs w-5 h-5 rounded-full bg-blue-500 text-white absolute -right-2 -top-2 flex items-center justify-center">
                 0
@@ -53,6 +63,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      {isMenuOpen && <CartMenu />}
     </section>
   );
 };

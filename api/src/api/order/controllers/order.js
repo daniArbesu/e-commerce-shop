b3e2cb6c19bba22f57a04673e8a7e28969657aa6
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-13 01:48:32 +0200

    feat: add stripe order handle

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -11,26 +11,27 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
     const { products } = ctx.request.body;
 
-    const lineItems = await Promise.all(
-      products.map(async (product) => {
-        const item = await strapi
-          .service("api::product.product")
-          .findOne(product.id);
-
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: item.title,
-            },
-            unit_amount: item.price * 100,
-          },
-          quantity: item.quantity,
-        };
-      })
-    );
     try {
-      const session = stripe.checkout.create({
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::product.product")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.title,
+              },
+              unit_amount: Math.round(item.price * 100),
+            },
+            quantity: product.quantity,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
         mode: "payment",
         success_url: `${process.env.CLIENT_URL}?success=true`,
         cancel_url: `${process.env.CLIENT_URL}?success=false`,
@@ -39,6 +40,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         payment_method_types: ["card"],
       });
 
+      console.log(session);
+
       await strapi
         .service("api::order.order")
         .create({ data: { products, stripeId: session.id } });
@@ -46,7 +49,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       return { stripeSession: session };
     } catch (error) {
       ctx.response.status = 500;
-      return error;
+      return { error };
     }
   },
 }));
